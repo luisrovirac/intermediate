47fb79c893ae372485192d9189d3739175eabfca
luisrovirac <luisrovirac@gmail.com>
2024-02-02 09:27:08 -0400

    return all data

--- app/Http/Controllers/OpenaixController.php
+++ app/Http/Controllers/OpenaixController.php
@@ -76,6 +76,6 @@ class OpenaixController extends Controller
 				'top_p' => 1.0,
 
 			])->json();
-			return response()->json($data['choices'][0]['message'],200,[]);
+			return response()->json($data,200,[]);
 	}
 }
