282158ef05e2bf474c307c3a26c6a1a93550099a
luisrovirac <luisrovirac@gmail.com>
2024-02-02 09:19:55 -0400

    Edit Controller by first throw

--- app/Http/Controllers/OpenaixController.php
+++ app/Http/Controllers/OpenaixController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Openaix;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OpenaixController extends Controller
 {
@@ -57,6 +58,24 @@ class OpenaixController extends Controller
     }
 
 	public function getopenai() {
-		return response()->json('Works', 200, []);
+		$search = "who is google";
+
+		$data = Http::withHeaders([
+			'Content-Type' => 'application/json',
+			'Authorization' => 'bearer ',env('OPEN_API_KEY')
+		])->post(
+			'https://api.openai.com/v1/chat/completions',[
+				'model' => 'gpt-3.5-turbo',
+				'messages' => [
+					[
+						'role' => 'user', 'content' => $search
+					]
+				],
+				'temperature' => 0.5,
+				'max_tokens' => 200,
+				'top_p' => 1.0,
+
+			])->json();
+			return response()->json($data['choices'][0]['message'],200,array(),JSON_PRETTY_PRINT);
 	}
 }
