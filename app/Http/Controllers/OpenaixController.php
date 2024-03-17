d7a41e28f8d6edc4815005a907d3e53b5590b35a
luisrovirac <luisrovirac@gmail.com>
2024-03-17 21:01:35 +0100

    step 4.11 consult endpoint chats/items

--- app/Http/Controllers/OpenaixController.php
+++ app/Http/Controllers/OpenaixController.php
@@ -248,9 +248,9 @@ echo "Linea 112......";
 			])->get($endpointchats)->json();
 				//$newdata = json_encode($data);
 				$newdata = json_decode($data);
-				//return response()->json($newdata,200,[]);
+				return response()->json($newdata,200,[]);
 				//return response()->json($data->{"messages"},200,[]);
-				return response()->json($newdata->{"messages"},200,[]);
+				//return response()->json($newdata->{"messages"},200,[]);
 				//return response()->json($data,200,[]);
 			} catch (\Throwable $th) {
 				return response()->json($th,200,[]);
