b708207dd5ed27a089798163fb6f54fe97d338fa
luisrovirac <luisrovirac@gmail.com>
2024-03-17 23:00:16 +0100

    Intent 001 read messages of json result

--- app/Http/Controllers/OpenaixController.php
+++ app/Http/Controllers/OpenaixController.php
@@ -248,7 +248,7 @@ echo "Linea 112......";
 			])->get($endpointchats)->json();
 				//$newdata = json_encode($data);
 				$newdata = json_decode($data);
-				return response()->json($newdata[0],200,[]);
+				return response()->json($newdata);
 				//return response()->json($newdata,200,[]);
 				//return response()->json($data->{"messages"},200,[]);
 				//return response()->json($newdata->{"messages"},200,[]);
