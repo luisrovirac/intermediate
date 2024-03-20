33d597d605006ec3ff5b0040e92d809fb88a4035
luisrovirac <luisrovirac@gmail.com>
2024-03-20 13:52:19 +0100

    arrow test completions to openai 031

--- app/Http/Controllers/OpenaixController.php
+++ app/Http/Controllers/OpenaixController.php
@@ -280,7 +280,7 @@ echo "";
 //echo $data[0];
 //echo $data[0]["choices"];
 //echo $data[0]["choices"][0];
-echo $data[0]->choices;
+echo json_decode($data);
 //echo response()->json($data[0]['choices'][0]['message']['content']);
 
 /*
