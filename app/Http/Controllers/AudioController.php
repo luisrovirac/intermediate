240d71d5c412d9ed6081427c95b3e2b1dc9db7c3
Luis Manuel Rovira Cruz <luisrovirac@gmail.com>
2024-07-08 11:44:37 +0200

    change texttospeech by lost mp3 to end - arrow 3

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -38,8 +38,8 @@ class AudioController extends Controller
     		// Request parameters
     		$params = [
         		'model' => 'tts-1',
-        		//'input' => "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-        		'input' => $request->input,
+        		'input' => "This is there are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words",
+        		//'input' => $request->input,
         		'voice' => 'alloy',
     		];
 
@@ -53,7 +53,7 @@ class AudioController extends Controller
     		]);
 
     		// Save the audio to a file
-    		file_put_contents('speech.mp3', $response->getBody());
+    		file_put_contents('newspeech.mp3', $response->getBody());
 
     		echo "Audio generated successfully!\n";
 		} catch (\Exception $e) {
