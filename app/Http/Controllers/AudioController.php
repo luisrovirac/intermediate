f9785ddd454266c4ee500b2354859db66599d1d9
Luis Manuel Rovira Cruz <luisrovirac@gmail.com>
2024-07-09 12:35:26 +0200

    texttospeech change for will accepted more character in the input

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -42,7 +42,8 @@ class AudioController extends Controller
 
     		// Request parameters
     		$params = [
-        		'model' => 'tts-1',
+        		//'model' => 'tts-1',
+				'model' => 'whisper-1',
         		//'input' => "This is there are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words",
         		'input' => $request->input,
         		'voice' => 'alloy',
