0b0508fd7275d9fd4a3c882d2d955f84d36e34a2
Luis Manuel Rovira Cruz <luisrovirac@gmail.com>
2024-09-17 20:06:56 +0200

    upload files img to aws step 03 - edit model to add photo01 to photo05

--- app/Models/Assistant.php
+++ app/Models/Assistant.php
@@ -9,7 +9,7 @@ class Assistant extends Model
 {
     use HasFactory;
 
-	protected $fillable = ['name', 'details','infoLoraIni','infoLoraEnd','typesex_id','voice'];
+	protected $fillable = ['name', 'details','infoLoraIni','infoLoraEnd','typesex_id','voice','photo01','photo02','photo03','photo04','photo05'];
 
 	protected $hidden = ['created_at', 'updated_at'];
 }
