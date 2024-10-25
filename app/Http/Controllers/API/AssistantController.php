fd6bb04257101e34aca31e206978dac0f7731dde
Luis Manuel Rovira Cruz <luisrovirac@gmail.com>
2024-10-25 11:14:22 +0200

    First steps by create images for new asisstant 09

--- app/Http/Controllers/API/AssistantController.php
+++ app/Http/Controllers/API/AssistantController.php
@@ -259,7 +259,7 @@ class AssistantController extends Controller
 			return response()->json($data['message'], $data['status']);				
 		}
 		
-        $response = Http::post('https://8ec3-80-102-129-53.ngrok-free.app/v1/generation/text-to-image',$request);
+        $response = Http::post('https://0af2-80-102-129-53.ngrok-free.app/v1/generation/text-to-image',$request);
         return $response;
 
 		try {
